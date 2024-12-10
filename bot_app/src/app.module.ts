be918e11af56b8b1ca59a80486230feaf157171c
sageak <52025563+sageak@users.noreply.github.com>
2024-12-10 13:55:57 +0530

    Update app.module.ts

--- bot_app/src/app.module.ts
+++ bot_app/src/app.module.ts
@@ -8,7 +8,7 @@ import { BotModule } from './bot/bot.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://johntharianmec:NQy88gOAa51jK3p4@searchdb.fefjva0.mongodb.net/telegram_bot?retryWrites=true&w=majority',
+      'mongodb+srv://akash:NQy88gOAa51jK3p4@searchdb.fefjva0.mongodb.net/telegram_bot?retryWrites=true&w=majority',
     ),
     UserModule,
     BotModule,
